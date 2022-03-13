e6e506e603f16d9cb11fb980b0bd5f250755c1fc
Sahilpanchal2903 <sahilpanchal2903@gmail.com>
2022-03-13 13:44:19 +0000

    documented the code, added a condition where it would delete all the keys if no key is found with the correct IP.

--- src/main/java/Core/KeyManagers/KeyChecker.java
+++ src/main/java/Core/KeyManagers/KeyChecker.java
@@ -12,21 +12,55 @@ import java.util.Arrays;
 import java.util.List;
 
 public class KeyChecker extends Util {
-    private Keys mapKeys(String username, String password) throws AuthException, IOException {
+    /**
+     * This method returns the json as class for ease of use
+     * @param username username of the user
+     * @param password password associated with the above username
+     * @return an Existing Keys available for the account
+     * @throws AuthException thrown on wrong username and password
+     * @throws IOException thrown by gson
+     */
+    private ExistingKeyModel mapKeys(String username, String password) throws AuthException, IOException {
         String keys = new KeyHandler().getKeys(username, password);
-        return deserialize(keys, Keys.class);
+        return deserialize(keys, ExistingKeyModel.class);
     }
 
+    /**
+     * this method calls the http method to revoke all the keys
+     * @param keys array of keys present for the user
+     * @param username username of the user
+     * @param password password associated with the username
+     * @throws AuthException thrown on wrong username and password
+     */
+    private void revokeKeys(ExistingKeyModel.Key[] keys, String username, String password) throws AuthException {
+        new KeyHandler().deleteAllKeys(keys, username, password);
+    }
+
+    /**
+     * this is used map the json of newly created keys to a <code>KeyModel</code>
+     * @param json String for
+     * @return
+     * @throws IOException
+     */
     private KeyModel mapKeys(String json) throws IOException {
         return deserialize(json, KeyModel.class);
     }
 
+    /**
+     * to get the valid keys of an account
+     * @param username username of the user
+     * @param password password associated with the above username
+     * @return a list of valid keys
+     * @throws AuthException thrown on wrong username and password
+     * @throws IOException thrown by gson
+     */
+
     public List<String> getValidKeys(String username, String password) throws AuthException, IOException {
         String ip = getIP();
-        Keys keys = mapKeys(username, password);
+        ExistingKeyModel existingKeyModel = mapKeys(username, password);
         List<String> validKeys = new ArrayList<>();
 
-        Arrays.stream(keys.getKeys()).forEach(keyObj -> {
+        Arrays.stream(existingKeyModel.getKeys()).forEach(keyObj -> {
             Arrays.stream(keyObj.getIps()).forEach(ips -> {
                 if (ips.equalsIgnoreCase(ip))
                     validKeys.add(keyObj.getKey());
@@ -34,12 +68,21 @@ public class KeyChecker extends Util {
         });
 
         if (validKeys.isEmpty()) {
+            //Delete all the keys when no key is valid
+            if (existingKeyModel.getKeys().length >= 10) {
+                revokeKeys(existingKeyModel.getKeys(), username, password);
+            }
             String createdKeys = new KeyHandler().createKeys(username, password, ip);
             validKeys.add(mapKeys(createdKeys).getKeys().getKey());
         }
         return validKeys;
     }
 
+    /**
+     * to get the IP of the machine
+     * @return the IP of the machine
+     * @throws IOException
+     */
     private String getIP() throws IOException {
         java.net.URL url = new URL("http://checkip.amazonaws.com");
         BufferedReader in = new BufferedReader(new InputStreamReader(
