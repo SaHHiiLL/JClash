5c3f79588c97ad9114a97d2c52471f8400475ea3
Sahilpanchal2903 <sahilpanchal2903@gmail.com>
2022-03-19 21:39:57 +0000

    git disaster

--- src/main/java/Core/KeyManagers/KeyHandler.java
+++ src/main/java/Core/KeyManagers/KeyHandler.java
@@ -1,146 +1,92 @@
 package Core.KeyManagers;
 
+import Core.Util;
 import Core.exception.AuthException;
 
-import java.net.CookieManager;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class KeyHandler {
-
+public class KeyHandler extends Util {
     /**
-     * Log in for the user
+     * This method returns the json as class for ease of use
      * @param username username of the user
      * @param password password associated with the above username
-     * @return a <code>HttpClient.Builder</code> which contains the cookie
+     * @return an Existing Keys available for the account
+     * @throws AuthException thrown on wrong username and password
+     * @throws IOException thrown by gson
+     */
+    private ExistingKeyModel mapKeys(String username, String password) throws AuthException, IOException {
+        String keys = new KeysHttpRequest().getKeys(username, password);
+        return deserialize(keys, ExistingKeyModel.class);
+    }
+
+    /**
+     * this method calls the http method to revoke all the keys
+     * @param keys array of keys present for the user
+     * @param username username of the user
+     * @param password password associated with the username
      * @throws AuthException thrown on wrong username and password
      */
-    protected HttpClient.Builder login(String username, String password) throws AuthException {
+    private void revokeKeys(ExistingKeyModel.Key[] keys, String username, String password) throws AuthException {
+        new KeysHttpRequest().deleteAllKeys(keys, username, password);
+    }
 
-        HttpClient.Builder client = HttpClient.newBuilder();
-        CookieManager cookieManager = new CookieManager();
+    /**
+     * this is used map the json of newly created keys to a <code>KeyModel</code>
+     * @param json String for
+     * @return
+     * @throws IOException
+     */
+    private KeyModel mapKeys(String json) throws IOException {
+        return deserialize(json, KeyModel.class);
+    }
 
-        HttpRequest.BodyPublisher bodyPublisher = HttpRequest.BodyPublishers
-                .ofString("{\"email\":\""+username+"\",\"password\":\""+password+"\"}");
-        HttpRequest request = HttpRequest.newBuilder(
-                        URI.create("https://developer.clashofclans.com/api/login")
-                )
-                .header("authorization", "")
-                .header("Content-Type", "application/json; charset=utf-8")
-                .POST(bodyPublisher).build();
+    /**
+     * to get the valid keys of an account
+     * @param username username of the user
+     * @param password password associated with the above username
+     * @return a list of valid keys
+     * @throws AuthException thrown on wrong username and password
+     * @throws IOException thrown by gson
+     */
 
-        AtomicInteger statusCode = new AtomicInteger();
-        client.cookieHandler(cookieManager)
-                .build()
-                .sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::statusCode)
-                .thenAccept(statusCode::set)
-                .join();
-        if (statusCode.get() != 200){
-            throw new AuthException("Email or Password is incorrect");
+    public List<String> getValidKeys(String username, String password) throws AuthException, IOException {
+        String ip = getIP();
+        ExistingKeyModel existingKeyModel = mapKeys(username, password);
+        List<String> validKeys = new ArrayList<>();
+
+        Arrays.stream(existingKeyModel.getKeys()).forEach(keyObj -> {
+            Arrays.stream(keyObj.getIps()).forEach(ips -> {
+                if (ips.equalsIgnoreCase(ip))
+                    validKeys.add(keyObj.getKey());
+            });
+        });
+
+        if (validKeys.isEmpty()) {
+            //Delete all the keys when no key is valid
+            if (existingKeyModel.getKeys().length >= 10) {
+                revokeKeys(existingKeyModel.getKeys(), username, password);
+            }
+            String createdKeys = new KeysHttpRequest().createKeys(username, password, ip);
+            validKeys.add(mapKeys(createdKeys).getKeys().getKey());
         }
-        return client;
+        return validKeys;
     }
 
     /**
-     * get all the existing keys for the user
-     * @param username username of the user
-     * @param password password associated with the above username
-     * @return a Json string which can be converted to a class
-     * @throws AuthException thrown on wrong username and password
+     * to get the IP of the machine
+     * @return the IP of the machine
+     * @throws IOException
      */
-    protected String getKeys(String username, String password) throws AuthException {
-        HttpClient.Builder client = login(username, password);
-
-        AtomicReference<String> ssl = new AtomicReference<>("nothing");
-        HttpRequest.BodyPublisher bodyPublisher1 = HttpRequest.BodyPublishers
-                .ofString("");
-
-        HttpRequest request1 = HttpRequest.newBuilder(
-                        URI.create("https://developer.clashofclans.com/api/apikey/list"))
-                .header("Content-Type", "application/json; charset=utf-8")
-                .POST(bodyPublisher1).build();
-
-        client.build()
-                .sendAsync(request1, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(ssl::set)
-                .join();
-
-        return ssl.get();
-    }
-
-    /**
-     * Creates a single key for the user
-     * @param username username of the user
-     * @param password password associated with the above username
-     * @param ip , ip address for the machine which will be accessing this lib
-     * @return a Json string which can be converted to a class
-     * @throws AuthException thrown on wrong username and password
-     */
-    protected String createKeys(String username, String password, String ip) throws AuthException {
-        HttpClient.Builder client = login(username, password);
-        String json ="{\n" +
-                "                    \"cidrRanges\":\n" +
-                "                     [\"%s\"],\n" +
-                "                      \"description\": \"Api key created by JClash on %s\",\n" +
-                "                      \"name\": \"JClash\",\n" +
-                "                      \"scopes\": [\"clash\"]\n" +
-                "                }";
-        HttpRequest.BodyPublisher bodyPublisher1 = HttpRequest.BodyPublishers
-                .ofString(String.format(json, ip, LocalDateTime.now().atZone(ZoneId.systemDefault()).toString()));
-
-        HttpRequest request = HttpRequest.newBuilder(
-                        URI.create("https://developer.clashofclans.com/api/apikey/create"))
-                .header("Content-Type", "application/json; charset=utf-8")
-                .POST(bodyPublisher1).build();
-
-        AtomicReference<String> stringAtomicReference = new AtomicReference<>();
-
-        client.build().sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(stringAtomicReference::set)
-                .join();
-        return stringAtomicReference.get();
-    }
-
-    /**
-     * Deletes all the key from the user
-     * @param keys list of keys available for the user
-     * @param username username of the user
-     * @param password password associated with the above username
-     * @throws AuthException thrown on wrong username and password
-     */
-    protected void deleteAllKeys (ExistingKeyModel.Key[] keys, String username, String password) throws AuthException {
-        HttpClient.Builder client = login(username, password);
-        String json = "{\n" +
-                "                        \"id\": \"%s\"\n" +
-                "                    }";
-        for (ExistingKeyModel.Key key : keys) {
-            deleteKey(client, json, key);
-        }
-    }
-
-    /**
-     * deleted a single key for the user
-     * @param client http builder
-     * @param json POST request JSON
-     * @param key needs to be deleted
-     */
-    private void deleteKey(HttpClient.Builder client, String json, ExistingKeyModel.Key key) {
-        HttpRequest.BodyPublisher bodyPublisher = HttpRequest.BodyPublishers.ofString(String.format(json, key.getId()));
-
-        HttpRequest request = HttpRequest.newBuilder(
-                        URI.create("https://developer.clashofclans.com/api/apikey/revoke"))
-                .header("Content-Type", "application/json; charset=utf-8")
-                .POST(bodyPublisher)
-                .build();
-        client.build().sendAsync(request, HttpResponse.BodyHandlers.ofString()).join();
+    private String getIP() throws IOException {
+        java.net.URL url = new URL("http://checkip.amazonaws.com");
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                url.openStream()));
+        return in.readLine();
     }
 }
